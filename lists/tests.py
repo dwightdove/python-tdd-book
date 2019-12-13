fe084d0e582c692d6aa1ef136f5a521da6a090a6
El Negriti <dwightdove@gmail.com>
2019-12-13 23:38:31 +0300

    Commit prior to refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -3,18 +3,13 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 
-class HomePageTest(TestCase):
-    # 1. make sure view is returned
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-    
-    # 2. make sure correct html is returned
-    def test_home_page_returns_correct_html(self):
+class HomePageTest(TestCase):    
+    # 1. make sure correct html is returned
+    def test_uses_home_template(self):
         response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed('home.html')
