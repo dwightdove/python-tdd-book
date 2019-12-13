3745e14e87a8fa7f8af79a91f9bf428fd2cb261d
El Negriti <dwightdove@gmail.com>
2019-12-13 18:09:20 +0300

    Function test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -4,6 +4,8 @@ be a slow process to build muscle-reflex in approaching development in a new lig
 '''
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -19,17 +21,26 @@ class NewVisitorTest(unittest.TestCase):
 
         # she notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_elements_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+
+        # She type "Buy peacock feathers" into a text box
+        inputbox.send_keys('Buy peacok feathers')
+
+        # When she hits enter, the page updaes, and not the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
         self.fail('Finish the test!')
-        assert 'To-Do' in self.browser.title
-
-# She is invited to enter a to-do item straight away
-
-# She type "Buy peacock feathers" into a text box
-
-# When she hits enter, the page updaes, and not the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
-
-# There is still a tect box inviting her to add another item. She enters "Use peacock feathers to make a fly"
+# There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
 
 # The page updates again, and now shows both items on her list
 
